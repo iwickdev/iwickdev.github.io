8833e7fef2cc2e64ad5a44073542a3ce819469a6
IWick <smarttecgaming@gmail.com>
2024-04-10 18:00:38 -0500

    Added blogs :wow:

---
+++ js/blog-render.js
@@ -0,0 +1,46 @@
+const render_area = document.getElementById("blog-area")
+let url_arguments = new URLSearchParams(window.location.search)
+
+async function renderBlog(page) {
+    let response = await fetch(`/blogs/${page}.html`)
+    let html_content = await response.text()
+
+    if (response.status === 404) {
+        return `<p>Blog not found on server, check UUID.</p>`
+    }
+    return html_content
+}
+
+document.querySelectorAll("#nav-bar p").forEach((element) => {
+    element.addEventListener("click", () => {
+        let blog_id = element.getAttribute("uuid")
+        let blog_created = element.getAttribute("created")
+        let blog_updated = element.getAttribute("updated")
+        let blog_tags = element.getAttribute("tags")
+
+
+        renderBlog(blog_id).then((value) => {
+            render_area.innerHTML = `
+                    <h1>${element.innerText}</h1>
+                    <p>Created ${blog_created} / Updated ${blog_updated}</p>
+                    <p>${blog_tags}</p>
+                    <hr>
+                    `
+
+            render_area.innerHTML += value
+
+            window.history.replaceState(
+                {},
+                "IWick Blog",
+                window.location.origin + window.location.pathname + `?blog=${blog_id}`
+            )
+        })
+    })
+})
+
+if ( url_arguments.get("blog") ) {
+    let elm = document.querySelector(`#nav-bar p[uuid=${url_arguments.get("blog")}]`)
+    if (elm) {elm.click()}
+} else {
+    document.querySelector("#nav-bar p").click()
+}
